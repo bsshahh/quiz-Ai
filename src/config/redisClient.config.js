4538da0361aa7795dca782b982574f140ca06fcf
bsshahh <shahbhavya338@gmail.com>
2025-09-16 18:16:27 +0530

    file

--- src/config/redisClient.config.js
+++ src/config/redisClient.config.js
@@ -1,9 +1,10 @@
 import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
-const redis = new Redis(
-  process.env.REDIS_URL         
-);
+const redis = new Redis({
+  host: process.env.REDIS_HOST,   
+  port: process.env.REDIS_PORT,          
+});
 
 redis.on("connect", () => console.log("✅ Connected to Redis"));
 redis.on("error", (err) => console.error("❌ Redis error:", err));
