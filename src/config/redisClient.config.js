420cd39086198df648a3782616a199179353f768
bsshahh <shahbhavya338@gmail.com>
2025-09-16 23:14:37 +0530

    newm

--- src/config/redisClient.config.js
+++ src/config/redisClient.config.js
@@ -1,9 +1,8 @@
 import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
-const redis = new Redis({
-  host: process.env.REDIS_HOST,   
-  port: process.env.REDIS_PORT,          
+const redis = new Redis(process.env.REDIS_URL, {
+  tls: {}, // Upstash requires TLS
 });
 
 redis.on("connect", () => console.log("✅ Connected to Redis"));
